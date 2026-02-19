7d7506a6169ad76e93059bd6db6dd067f318d130
JesseIngles <JesseIngles@hotmail.com>
2026-02-19 10:52:11 +0100

    inserted many data

--- webapi/obj/Debug/net10.0/webapi.AssemblyInfo.cs
+++ webapi/obj/Debug/net10.0/webapi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("webapi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d27dfaa04a32a3fe78868f5e59352f312faf606")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+41b1647ba7e6be40f1d1f5ce322c22efed14f1c3")]
 [assembly: System.Reflection.AssemblyProductAttribute("webapi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("webapi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
